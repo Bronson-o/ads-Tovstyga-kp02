270f35c61fcd7c73308bfb3fedc858b1414c4fd3
Bronson-o <71923117+Bronson-o@users.noreply.github.com>
2020-10-30 08:29:29 +0200

    .

---
+++ labs/lab1/Завдання 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+          
+             Console.WriteLine("Enter x");  
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y");  
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter z");  
+            double z = double.Parse(Console.ReadLine());
+
+            if ( y!=1 && z!=0 && y!=0 )
+            {
+                double a = Math.Pow((x + y*y + 2*z), 1/3) / Math.Abs(1-y)*z*z;
+                Console.WriteLine("a = {0}",a);
+
+                double b = Math.Sin((x*x)/a + y*Math.Pow(z,3));
+                Console.WriteLine("b = {0}",b);
+
+
+            }
+            else
+            Console.WriteLine("Вираз не задовольняє ОДЗ");
+        }
+    }
+
