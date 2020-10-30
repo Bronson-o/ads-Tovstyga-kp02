267a4766b58dd664bdccdc22f6a09bb58500f034
Bronson-o <71923117+Bronson-o@users.noreply.github.com>
2020-10-30 12:57:43 +0200

    Finished

--- labs/lab1/Завдання 1/Program.cs
+++ labs/lab1/Завдання 1/Program.cs
@@ -15,18 +15,23 @@
             Console.WriteLine("Enter z");  
             double z = double.Parse(Console.ReadLine());
 
-            if ( y!=1 && z!=0 && y!=0 )
+            if ( y!=1 && z!=0)
             {
-                double a = Math.Pow((x + y*y + 2*z), 1/3) / Math.Abs(1-y)*z*z;
+                double a = Math.Pow(x + y*y + 2*z, (1/3)) / (Math.Abs(1-y)*(z*z));
                 Console.WriteLine("a = {0}",a);
 
+                if (a==0)
+                {
+                   Console.WriteLine("При а=0 обчислити b неможливо");
+                }
+                else
+                {
                 double b = Math.Sin((x*x)/a + y*Math.Pow(z,3));
                 Console.WriteLine("b = {0}",b);
-
-
+                }
             }
             else
-            Console.WriteLine("Вираз не задовольняє ОДЗ");
+            Console.WriteLine("Значення не задовольняють ОДЗ");
         }
     }
 
